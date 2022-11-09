d58babe08cc5e923aeff9912799894304c6686d3
Adam <admedh@outlook.fr>
2022-11-09 11:20:04 +0100

    Implémentation de API/Etudiant.java dans EtudiantP.java

--- src/fr/iutfbleau/projetIHM2022FI2/MP/EtudiantP.java
+++ src/fr/iutfbleau/projetIHM2022FI2/MP/EtudiantP.java
@@ -1,58 +1,57 @@
 package fr.iutfbleau.projetIHM2022FI2.MP;
+
 import fr.iutfbleau.projetIHM2022FI2.API.*;
 import java.sql.*;
 import java.util.*;
 
 /**
- * Un étudiant
+ * L'implémentation de l'interface Etudiant.java de l'API
+ * @see fr.iutfbleau.projetIHM2022FI2.API.Etudiant.java
+ *
+ * @author Adam Meddahi
+ * @author Youcef Sayad
+ * @author Yannis Bouarroudj
  */
-
 public class EtudiantP implements Etudiant {
 
-    int id;
-    Connection cnx = DriverManager.getConnection("jdbc:mariadb://dwarves.iut-fbleau.fr/sayad", "sayad", "sayadpro");
-    PreparedStatement pst;
+    private int id;
+    private String nom;
+    private String prenom;
 
-    public EtudiantP(String nom, String prenom) throws SQLException {
-        try {
-            pst = cnx.prepareStatement("INSERT INTO `Etudiants`(`Nom`, `Prenom`) VALUES (" + nom + "," + prenom + ")");
-            pst.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-    }
-
-    public int getId() {
-        try {
-            pst = cnx.prepareStatement("SELECT `id`, `Nom`, `Prenom` FROM `Etudiants` WHERE 1");
-            pst.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        
-        return 0;
-    }
-
-    public String getNom() {
-        try {
-            pst = cnx.prepareStatement("SELECT `id`, `Nom`, `Prenom` FROM `Etudiants` WHERE 1");
-            pst.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        
-        return null;
-    }
-
-    public String getPrenom() {
-        try {
-            pst = cnx.prepareStatement("SELECT `id`, `Nom`, `Prenom` FROM `Etudiants` WHERE 1");
-            pst.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        
-        return null;
+    /**
+     * Constructeur de la classe étudiant, la factory fera tout le travail relatif à la base de données
+     * @param nom - Le nom de l'étudiant
+     * @param prenom - Le prénom de l'étudiant
+     * @param id - L'identifiant de l'étudiant dans la base de données
+     */
+    public EtudiantP(String nom, String prenom, int id){
+        //On crée un nouvel etudiant dans la table des étudiant
+        this.nom = nom;
+        this.prenom = prenom;
+        this.id = id;
     }
     
+    /**
+     * permet de récupérer l'identifiant de l'étudiant.
+     * @return l'identifiant.
+     */   
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * permet de récupérer le nom de l'étudiant
+     * @return le nom de l'étudiant.
+     */
+    public String getNom() {        
+        return this.nom;
+    }
+
+    /**
+     * permet de récupérer le prénom de l'étudiant
+     * @return le prénom de l'étudiant
+     */
+    public String getPrenom() {
+        return this.prenom;
+    }
 }
