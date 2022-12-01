9547ff43e223be133836643a71c9aeee3d5d468f
bouarrou <bouarrou@localhost>
2022-12-01 11:01:40 +0100

    pas de changement

--- src/fr/iutfbleau/projetIHM2022FI2/Vues/Admin/Menu.java
+++ src/fr/iutfbleau/projetIHM2022FI2/Vues/Admin/Menu.java
@@ -13,31 +13,47 @@ public class Menu extends JFrame {
         gbc.ipady =  GridBagConstraints.CENTER; // definie la largeur des élements
         gbc.ipadx =  GridBagConstraints.CENTER; // définie la longueur des éléments
         gbc.anchor = GridBagConstraints.CENTER; // définie le type d'espacement entre les element
-        gbc.weightx = 2;// définie le nombre d'elements horizontale
-        gbc.weighty = 1;// définie le nombre d'élements verticale
+        gbc.weightx = 5;// définie le nombre d'elements horizontale
+        gbc.weighty = 2;// définie le nombre d'élements verticale
 
-        gbc.gridx = 1;
-        gbc.gridy = 1;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
         gbc.gridwidth = 1;
-        gbc.gridheight = 1;
+        gbc.gridheight = 2;
         JPanel table = new JPanel();
         table.setBackground(new Color(125,0,255));
         table.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 2, Color.BLACK));
         this.menu.add(table, gbc);
 
 
-
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 4;
+        gbc.gridheight = 1;
         JPanel table2 = new JPanel();
         table2.setBackground(Color.DARK_GRAY);
 
 
+
         //table2.setBackground(new Color(125,0,255));
         table2.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 2, Color.BLACK));
-        gbc.gridx = 2;
-        gbc.gridy = 1;
-        gbc.gridwidth = 3;
-        gbc.gridheight = 1;
         this.menu.add(table2, gbc);
+
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 4;
+        gbc.gridheight = 1;
+        JPanel carteGroupe = new JPanel();
+        this.menu.add(carteGroupe, gbc);
+
+        for (int i = 0; i < 2 ; i+=1 ) {
+            for (int j = 0; j < 4 ; j+=1 ) {
+            
+            }
+        }
+
+
 
     }
 }
