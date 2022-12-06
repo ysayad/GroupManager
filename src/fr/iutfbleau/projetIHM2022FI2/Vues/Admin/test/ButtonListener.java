4ee3d6f55f350be3078bfd53e3667c8c89868a54
bouarrou <bouarrou@localhost>
2022-12-06 12:44:56 +0100

    Groupe affiche en non persistant

--- src/fr/iutfbleau/projetIHM2022FI2/Vues/Admin/test/ButtonListener.java
+++ src/fr/iutfbleau/projetIHM2022FI2/Vues/Admin/test/ButtonListener.java
@@ -39,6 +39,7 @@ public class ButtonListener implements MouseListener{
 
     public void refresh(String name){
 
+            if(name == "Groupes    "){
             this.window.remove(menu);
 
 
@@ -72,6 +73,42 @@ public class ButtonListener implements MouseListener{
             this.window.repaint();
             this.window.invalidate();
             this.window.validate();
+            }
+            if (name == "Etudiants  ") {
+                            this.window.remove(menu);
+
+
+            Menu menu = new Menu(this.window, cardLayout, name, "Administrtateur");
+            GridLayout gridLayout = new GridLayout(1,2);
+            gridLayout.setHgap(0);
+            gridLayout.setVgap(0);
+
+
+            JPanel menuContainer = new JPanel(new BorderLayout());
+
+
+
+            JPanel menuP = new JPanel(new BorderLayout());
+            SearchBar searchbar = new SearchBar(menu,this.window);
+            menuP.add(searchbar.drawSearchBar(),BorderLayout.PAGE_START);
+            CarteEtudiant carteGroupe = new CarteEtudiant(menu,this.window);
+            menuP.add(carteGroupe.drawCarteGroupe());
+
+
+
+            menuContainer.add(menu.drawMenu(),BorderLayout.LINE_START);
+
+            menuContainer.add(menuP);
+
+
+            this.window.add(menuContainer, "Menu");
+
+
+            this.window.revalidate();
+            this.window.repaint();
+            this.window.invalidate();
+            this.window.validate();
+            }
 }
 
 
---
+++ src/fr/iutfbleau/projetIHM2022FI2/Vues/Admin/test/CarteEtudiant.java
@@ -0,0 +1,101 @@
+package fr.iutfbleau.projetIHM2022FI2.Vues.Admin.test;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.UIManager.*;
+import javax.swing.border.Border;
+import java.io.*;
+import java.lang.Thread;
+import java.awt.event.*;
+import java.util.*;
+
+import fr.iutfbleau.projetIHM2022FI2.API.Groupe;
+import fr.iutfbleau.projetIHM2022FI2.Controller.Cadmin;
+
+public class CarteGroupe extends JFrame {
+    Menu menu;
+    JFrame window;
+    public CarteGroupe(Menu menu, JFrame window){
+        this.menu = menu;
+        this.window = window;
+    }
+    
+    
+    public JPanel drawCarte(String name , string num){
+        JPanel carte = new JPanel(){
+        final ImageIcon icon = new ImageIcon(getClass().getResource("/carte-background.png"));
+      Image img = icon.getImage();
+      // initialiseur d'instance
+      {setOpaque(false);}
+      public void paintComponent(Graphics graphics) 
+      {
+        graphics.drawImage(img, 0, 0, this);
+        super.paintComponent(graphics); 
+      }
+    };
+
+
+      
+
+        carte.setLayout(new GridLayout(10,1));
+        carte.setPreferredSize(new Dimension(150,150));
+        carte.setSize(new Dimension(150,150));
+        carte.setMinimumSize(new Dimension(150,150));
+        carte.setBackground(Color.WHITE);
+        carte.add(new JLabel(""),BorderLayout.CENTER);
+        carte.add(new JLabel(""),BorderLayout.CENTER);
+        carte.add(new JLabel(""),BorderLayout.CENTER);
+        carte.add(new JLabel(""),BorderLayout.CENTER);
+        carte.add(new JLabel(""),BorderLayout.CENTER);
+        carte.add(new JLabel(""),BorderLayout.CENTER);
+        JLabel td = new JLabel(name);
+        td.setFont(new Font("Verdana", Font.PLAIN, 14));
+        td.setHorizontalAlignment(SwingConstants.CENTER);
+        carte.add(td,BorderLayout.CENTER);
+
+        carte.add(new JLabel(""),BorderLayout.CENTER);
+        JLabel nbetudiant = new JLabel(num+"/35 etudiant");
+        nbetudiant.setFont(new Font("Verdana", Font.PLAIN, 14));
+        nbetudiant.setHorizontalAlignment(SwingConstants.CENTER);
+        carte.add(nbetudiant,BorderLayout.CENTER);
+
+        FlowLayout layout = new FlowLayout();
+        layout.setVgap(0);
+        layout.setHgap(0);
+        JPanel test = new JPanel(layout);
+        test.setBackground(Color.WHITE);
+        test.add(carte);
+        //test.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.GRAY));
+        test.addMouseListener(new ButtonGroupeListener(test,this.menu,this.window));
+        return test;
+    }
+    
+
+    public JPanel drawCarteGroupe(){
+        JPanel cartecontainer = new JPanel();
+        //searchbarpanel.setBackground(Color.WHITE);
+
+
+Cadmin admin = Cadmin.Instance(false);
+Groupe grp = tu recup le groupe concerné
+for(Etudiant e : grp.getEtudiants()){
+
+         JPanel panneaucarte = new JPanel();
+         panneaucarte.add(drawCarte(e.getNom(),e.getPrenom()));
+
+        cartecontainer.add(panneaucarte);
+}
+
+
+
+Cadmin admin = Cadmin.Instance(false);
+Groupe grp = admin.getAllGroup().iterator().next();
+Set<Groupe> list = grp.getSousGroupes();
+for(Groupe g : list){
+         JPanel panneaucarte = new JPanel();
+         panneaucarte.add(drawCarte(g.getName(),g.getSize()));
+
+        cartecontainer.add(panneaucarte);
+}
+        return cartecontainer;
+    }
+}
