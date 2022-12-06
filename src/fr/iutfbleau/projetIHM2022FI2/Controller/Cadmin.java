092a0df00b3dc20a2959b2f4e0efc9619cf8e79b
Youcef Sayad <tabag@hotmail.fr>
2022-12-06 14:32:37 +0100

    fix fonction random name

--- src/fr/iutfbleau/projetIHM2022FI2/Controller/Cadmin.java
+++ src/fr/iutfbleau/projetIHM2022FI2/Controller/Cadmin.java
@@ -32,7 +32,7 @@ public class Cadmin {
 
             //init etudiant
             for (int i = 0; i < 50; i++) {
-                for(int j = 0; i < 5; i++) {
+                for(int j = 0; j < 5; i++) {
                     int index = random.nextInt(alphabet.length());
               
                     char randomChar = alphabet.charAt(index);
