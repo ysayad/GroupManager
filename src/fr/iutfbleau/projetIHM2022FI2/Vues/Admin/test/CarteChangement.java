4a7f79679ba067d966ffd4f2180aa2d3f0852b7f
KeaganZX <yanisbouarroudj148@gmail.com>
2022-12-08 19:24:51 +0100

    fin du design

--- src/fr/iutfbleau/projetIHM2022FI2/Vues/Admin/test/CarteChangement.java
+++ src/fr/iutfbleau/projetIHM2022FI2/Vues/Admin/test/CarteChangement.java
@@ -33,46 +33,55 @@ public class CarteChangement extends JFrame {
         this.carte = new JPanel();
         
 
+        JPanel txtc = new JPanel(new BorderLayout());
+        txtc.setOpaque(false);
+        txtc.setPreferredSize(new Dimension(600,50));
+        txtc.setSize(new Dimension(600,50));
+        txtc.setMaximumSize(new Dimension(800,50));
 
-
-      
+        JPanel buttonc = new JPanel();
+        buttonc.setOpaque(false);
 
         JLabel td = new JLabel(nomChangement);
-        this.carte.setPreferredSize(new Dimension(800,60));
-        this.carte.setSize(new Dimension(800,60));
-        this.carte.setMaximumSize(new Dimension(1000,60));
+        this.carte.setPreferredSize(new Dimension(600,60));
+        this.carte.setSize(new Dimension(600,60));
+        this.carte.setMaximumSize(new Dimension(800,60));
         this.carte.setBackground(Color.WHITE);
         td.setFont(new Font("Verdana", Font.PLAIN, 14));
         td.setHorizontalAlignment(SwingConstants.CENTER);
-        this.carte.add(td,BorderLayout.CENTER);
+        txtc.add(td, BorderLayout.CENTER);
+        this.carte.add(txtc,BorderLayout.CENTER);
 
         this.yes = new JButton("Oui");
         this.yes.setMinimumSize(new Dimension(50,50));
         this.yes.setMaximumSize(new Dimension(50,50));
         this.yes.setSize(50, 50);
         this.yes.setPreferredSize( new Dimension( 60, 50 ) );
-        this.yes.setBackground(Color.WHITE);
+        this.yes.setBackground(new Color(64,0,128));
+        this.yes.setForeground(Color.WHITE);
         this.yes.setFont(new Font("Verdana", Font.PLAIN, 15));
         this.yes.setName("Oui");
-        this.carte.add(this.yes);
+        this.yes.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        buttonc.add(this.yes);
 
         this.no = new JButton("Non");
         this.no.setMinimumSize(new Dimension(50,50));
         this.no.setMaximumSize(new Dimension(50,50));
         this.no.setSize(50, 50);
         this.no.setPreferredSize( new Dimension( 60, 50 ) );
-        this.no.setBackground(Color.WHITE);
+        this.no.setForeground(Color.WHITE);
+        this.no.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        this.no.setBackground(new Color(64,0,128));
         this.no.setFont(new Font("Verdana", Font.PLAIN, 15));
         this.no.setName("Non");
 
-        this.carte.add(this.no);  
+        buttonc.add(this.no);  
 
-        GridLayout layout = new GridLayout();
-        layout.setVgap(0);
-        layout.setHgap(0);
 
-        this.cartect = new JPanel(layout);
+        this.cartect = new JPanel(new BorderLayout());
         this.cartect.setBackground(Color.WHITE);
+        this.cartect.add(buttonc,BorderLayout.EAST);
         this.cartect.add(this.carte);
      
         //test.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.GRAY));
