3360d082e4494802120897ef03a45beec6759752
KeaganZX <yanisbouarroudj148@gmail.com>
2022-12-08 17:42:00 +0100

    retour depop

--- src/fr/iutfbleau/projetIHM2022FI2/Vues/Admin/test/ButtonGroupeCreerListener.java
+++ src/fr/iutfbleau/projetIHM2022FI2/Vues/Admin/test/ButtonGroupeCreerListener.java
@@ -62,6 +62,13 @@ public class ButtonGroupeCreerListener implements MouseListener {
             saisi_renommer.setBorder(border);
             saisi_renommer.setBorder(BorderFactory.createEmptyBorder(0,10,0,0));
             JButton creer = new JButton("Créer");
+            creer.setFont(new Font("Verdana", Font.PLAIN, 16));
+            creer.setBackground(new Color(64,0,128));
+            creer.setForeground(Color.WHITE);
+            creer.setFocusable(false);
+            creer.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+
             creer.addMouseListener(new CreerListener(creer,menu,cardLayout,window,g,dialog,saisi_renommer.getText()));
             JPanel saisi_container = new JPanel(new GridLayout(2,2));
 
